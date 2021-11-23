70dc7664144098104b7319a93e0e455a9adecc9d
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-11-23 14:53:19 -0300

    fix: starting input has rules to deal with bad entries.

--- src/pages/StartPage/index.js
+++ src/pages/StartPage/index.js
@@ -13,6 +13,10 @@ const StartPage = () => {
     const history = useHistory()
 
     const startQuizz = (ammount) => {
+        ammount = Math.floor(ammount)
+        if (ammount <= 0) {
+            ammount = ammount * -1
+        }
         callQuizz(ammount)
         history.push('/quizz')
     }
@@ -22,17 +26,14 @@ const StartPage = () => {
             <Typography variant="h6">How many questions?</Typography>
             <Typography gutterBottom variant="subtitle1">Choose an ammount and press start!</Typography>
             <div className="form_container">
-
-                <TextField required style={{ width: '150px' }}
+                <TextField type="number" required style={{ width: '150px' }}
                     size='small'
                     value={amm}
                     variant="outlined"
                     placeholder="Ex: 10"
                     onChange={(e) => setAmm(e.target.value)}
                 />
-
                 <Btn onClick={(e) => startQuizz(amm)}>Start</Btn>
-
                 <Btn onClick={() => history.push('/')}>Cancel</Btn>
             </div>
         </>
